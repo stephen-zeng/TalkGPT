8dd40bdca366a18cefad251b93fcb1b988082f00
Stephen Zeng <stephenzeng@goforit.top>
2024-12-19 09:48:27 +0800

    prepare websocket

--- backend/talkgpt/asgi.py
+++ backend/talkgpt/asgi.py
@@ -1,9 +1,10 @@
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from core import routings
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkgpt.settings')
 
-application = get_asgi_application()
+# 定义同步应用
+application = ProtocolTypeRouter({
+    'http': get_asgi_application()    
+})
