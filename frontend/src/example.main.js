56d486d0f9c82380e7f94157f48590efec9ade31
Stephen Zeng <stephenzeng@goforit.top>
2025-01-11 22:00:25 +0800

    add tip

---
+++ frontend/src/example.main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { io } from 'socket.io-client'
+import { Player } from '@/audio/player.js';
+import router from './router';
+import 'mdui/mdui.css'
+import 'mdui'
+import './assets/main.css'
+
+const app = createApp(App);
+const socket = io('wss://your-own-backend-server.com/');
+const player = new Player();
+app.provide('socket', socket);
+app.provide('player', player);
+app.use(router).mount("#app");
