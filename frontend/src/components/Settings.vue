91316bb293f515775f67ef2045ba996e80d3b1f7
Stephen Zeng <stephenzeng@goforit.top>
2025-01-11 21:50:58 +0800

    add tip

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -34,9 +34,9 @@
 </script>
 <template>
     <mdui-dialog :open=dialogStatus @overlay-click="cancelDialog()"
-    icon="settings" headline="Settings" close-on-overlay-click>
+    icon="settings" headline="Settings" close-on-overlay-click
+    description="Please click the connect button again after submit your credential">
         <mdui-text-field label="OpenAI API Key or your password" variant="outlined" clearable
-        description="Please click the connect button again after submit your credential"
         :value="key" @input="key=$event.target.value" type="password" toggle-password></mdui-text-field>
         <mdui-button slot="action" variant="outlined" @click="cancelDialog">Cancel</mdui-button>
         <mdui-button slot="action" @click="submit">Done</mdui-button>
